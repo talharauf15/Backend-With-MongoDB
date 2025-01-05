32e570e418f4c01b5be8714f3dea61de6b2bc561
talharauf15 <talharauf15@gmail.com>
2025-01-06 03:27:40 +0500

    Refactor MongoDB connection logic: move connection code to a new db/index.js file and simplify index.js by importing connectDB function.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
 
-
+connectDB();
 
 
 
@@ -14,23 +15,23 @@ import { DB_NAME } from "./constants.js";
 
 //this make index code messy
 
-import express from "express";
-import { error } from "console";
-const app = express();
+// import express from "express";
+// import { error } from "console";
+// const app = express();
 
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        app.on("error", () => {
-            console.log("Errror :", error);
-            throw error;
-        })
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         app.on("error", () => {
+//             console.log("Errror :", error);
+//             throw error;
+//         })
 
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error;
-    }
-})()
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// })()
