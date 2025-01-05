a642f844809006e2f05361d14b7c9b22e3a1c19b
talharauf15 <talharauf15@gmail.com>
2025-01-06 03:17:43 +0500

    Implement server setup with MongoDB connection in index.js

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+
+
+
+
+
+
+
+
+
+
+
+//this make index code messy
+
+import express from "express";
+import { error } from "console";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error", () => {
+            console.log("Errror :", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
